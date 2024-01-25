cc4aacce8a98aeb5a81a3802ef2ef2e50ac145c1
Neelumishra <neelumishra0397@gmail.com>
2024-01-25 12:01:24 +0530

    list key fix

--- components/form/List.tsx
+++ components/form/List.tsx
@@ -4,7 +4,7 @@ function List({ doctors }:any) {
   return (
    <>
     {
-        doctors?.map((e:any)=><li>{e.name}</li>)
+        doctors?.map((e:any,index:number)=><li key={index}>{e.name}</li>)
     }
    </>
   )
